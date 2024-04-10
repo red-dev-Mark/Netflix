16ff04d4fdc3a41e5cb129651567e4f3518ae98c
Mark <redhero8830@gmail.com>
2024-04-11 01:35:12 +0900

    feat: 필터 기능 추가

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -5,6 +5,7 @@ import { useSearchParams } from "react-router-dom";
 import { Alert, Spinner, Container, Row, Col } from "react-bootstrap";
 import MovieCard from "../../common/MovieCard/MovieCard";
 import ReactPaginate from "react-paginate";
+import { useMovieGenreQuery } from "../../hook/useMovieGenre";
 
 // 경로 2가지
 //1. nav바에서 클릭해서 온 경우 => 이런 경우는 PopularMovies 보여주기
@@ -15,6 +16,22 @@ import ReactPaginate from "react-paginate";
 
 export default function MoviePage() {
   const [page, setPage] = useState(1);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+
+  const { data: genreData } = useMovieGenreQuery();
+
+  // 장르 선택 핸들러
+  const handleGenreChange = (genreId) => {
+    setSelectedGenres((prevSelectedGenres) => {
+      const isGenreSelected = prevSelectedGenres.includes(genreId);
+
+      if (isGenreSelected) {
+        return prevSelectedGenres.filter((id) => id !== genreId);
+      } else {
+        return [...prevSelectedGenres, genreId];
+      }
+    });
+  };
 
   //keyword는 url의 쿼리에서 읽어옴
   const [query] = useSearchParams();
@@ -53,12 +70,39 @@ export default function MoviePage() {
       <Container>
         <Row>
           {/*필터 */}
-          <Col lg={4} xs={12}></Col>
+          <Col lg={4} xs={12}>
+            {genreData?.map((genre) => (
+              <div className="checkbox-container" key={genre.id}>
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  id={`genre-${genre.id}`}
+                  checked={selectedGenres.includes(genre.id)}
+                  onChange={() => handleGenreChange(genre.id)}
+                />
+                <label
+                  className="checkbox-label"
+                  htmlFor={`genre-${genre.id}`}
+                >
+                  {genre.name}
+                </label>
+              </div>
+            ))}
+          </Col>
           {/*영화 카드 */}
           <Col lg={8} xs={12}>
             <Row>
               {data?.results
                 .filter((movie) => movie.poster_path)
+                .filter((movie) => {
+                  return (
+                    selectedGenres.length === 0 ||
+                    movie.genre_ids.some((genreId) =>
+                      selectedGenres.includes(genreId)
+                    )
+                  );
+                })
+
                 .map((movie) => {
                   return (
                     <Col key={movie.id} lg={4} xs={12}>
@@ -66,6 +110,18 @@ export default function MoviePage() {
                     </Col>
                   );
                 })}
+
+              {/* {data?.results
+                .filter((movie) =>
+                  movie.genre_ids.some((genreId) =>
+                    selectedGenres.includes(genreId)
+                  )
+                )
+                .map((movie) => (
+                  <Col key={movie.id} lg={4} xs={12}>
+                    <MovieCard movie={movie} />
+                  </Col>
+                ))} */}
             </Row>
           </Col>
 
