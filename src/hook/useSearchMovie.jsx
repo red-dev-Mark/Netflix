8afa89cd2c15874c3b85199157af6001bb389f65
Mark <redhero8830@gmail.com>
2024-04-11 01:07:47 +0900

    feat: 페이지네이션 기능 추가

--- src/hook/useSearchMovie.jsx
+++ src/hook/useSearchMovie.jsx
@@ -5,19 +5,19 @@ import api from "../utils/api";
 //1. nav바에서 클릭해서 온 경우 => 이런 경우는 PopularMovies 보여주기
 //2. keyword를 입력해서 온 경우 => keyword와 관련된 영화들을 보여줌
 
-const fetchSearchMovie = ({ keyword }) => {
+const fetchSearchMovie = ({ keyword, page }) => {
   //TMDB - Search - Movie
   return keyword
-    ? api.get(`/search/movie?query=${keyword}`)
-    : api.get(`/movie/popular?language=ko`);
+    ? api.get(`/search/movie?query=${keyword}&page=${page}`)
+    : api.get(`/movie/popular?page=${page}?language=ko`);
 };
 
-export const useSearchMovieQuery = ({ keyword }) => {
+export const useSearchMovieQuery = ({ keyword, page }) => {
   return useQuery({
     //이번에는 조금 다름, 키워드에 따라서 url이 달라짐
-    queryKey: ["movie-search", keyword],
-    queryFn: () => fetchSearchMovie({ keyword }),
+    queryKey: ["movie-search", { keyword, page }],
+    // queryKey: ["movie-search",  keyword, page ], 이 방법도 가능
+    queryFn: () => fetchSearchMovie({ keyword, page }),
     select: (result) => result.data,
   });
 };
- 
--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import "./MoviePage.css";
 import { useSearchMovieQuery } from "../../hook/useSearchMovie";
 import { useSearchParams } from "react-router-dom";
 import { Alert, Spinner, Container, Row, Col } from "react-bootstrap";
 import MovieCard from "../../common/MovieCard/MovieCard";
+import ReactPaginate from "react-paginate";
 
 // 경로 2가지
 //1. nav바에서 클릭해서 온 경우 => 이런 경우는 PopularMovies 보여주기
@@ -12,12 +14,21 @@ import MovieCard from "../../common/MovieCard/MovieCard";
 //사실 이런 경우는 백엔드에서 하는 것이지만, TMDB에서 요청이 안되니 따로 해보자
 
 export default function MoviePage() {
+  const [page, setPage] = useState(1);
+
   //keyword는 url의 쿼리에서 읽어옴
   const [query] = useSearchParams();
   const keyword = query.get("q");
 
+  const handlePageClick = (page) => {
+    setPage(page.selected + 1);
+  };
+
   //만약 keyword가 없다면 null
-  const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword });
+  const { data, isLoading, isError, error } = useSearchMovieQuery({
+    keyword,
+    page,
+  });
   if (isLoading) {
     return (
       <div className="spinner-area">
@@ -35,7 +46,7 @@ export default function MoviePage() {
   if (!(data && data.results && data.results.length !== 0)) {
     return <div>No data available</div>;
   }
-  console.log(data);
+  //   console.log(data);
 
   return (
     <div>
@@ -57,6 +68,31 @@ export default function MoviePage() {
                 })}
             </Row>
           </Col>
+
+          <ReactPaginate
+            nextLabel="next"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={10}
+            marginPagesDisplayed={2}
+            pageCount={data.total_pages}
+            previousLabel="previous"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            breakLabel="..."
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            containerClassName="pagination"
+            activeClassName="active"
+            renderOnZeroPageCount={null}
+            //현재 페이지
+            //하지만 리액트 페이지네이션은 0부터 시작함
+            //따라서 -1을 해줘야함
+            forcePage={page - 1}
+          />
         </Row>
       </Container>
     </div>
