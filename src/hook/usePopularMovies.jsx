f65675956dc1c69e7daec7d1d863da713574d9aa
Mark <redhero8830@gmail.com>
2024-04-27 19:33:53 +0900

    [Style] 커스텀 훅 주석 추가

--- src/hook/usePopularMovies.jsx
+++ src/hook/usePopularMovies.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "../utils/api";
 
+// 인기 영화 커스텀 훅
 const fetchPopularMovies = () => {
   // return api.get(`/movie/popular`);
   return api.get(`/movie/popular?language=ko`);
