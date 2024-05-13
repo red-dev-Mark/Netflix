bcd8e3985c12df002ddd55600f24e16f9fde7293
Mark <redhero8830@gmail.com>
2024-05-13 22:42:24 +0900

    [style]커스텀 훅 관련 주석 추가

--- src/hook/useReviewMovies.jsx
+++ src/hook/useReviewMovies.jsx
@@ -2,12 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import api from "../utils/api";
 
 //영화 리뷰 커스텀 훅 추가
+//영화 리뷰 커스텀 훅 추가
 const fetchMovieReviews = ({ id }) => {
   return api.get(`/movie/${id}/reviews`);
 };
 export const useMovieReviewsQuery = ({ id }) => {
   return useQuery({
-    queryKey: ["movie-review", { id }],
+    queryKey: ["movie-review", {   id }],
     queryFn: () => fetchMovieReviews({ id }),
     select: (result) => result.data.results,
   });
